40a94f7b3d9ad761376f6b3dd0181b9cd1fe39e7
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-08-06 19:01:35 +0300

    zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+//сумму цифр в числе.
+
+
+void zadacha27 ()
+{
+     Console.WriteLine ("Введите число и нажмите 'Enter' ");
+    int numb = Convert.ToInt32(Console.ReadLine());
+
+     int sum = 0;
+     Console.Write("Сумма цифр числа " + numb);
+    while (numb > 0)
+    {
+        sum += numb%10;
+        numb /=10;
+    }
+    Console.WriteLine(" равна " + sum);
+}
+zadacha27 ();
