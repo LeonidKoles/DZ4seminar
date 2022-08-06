f1411ad7336f8de4b203c5d22b86d2d883bef666
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-08-06 18:38:32 +0300

    zadacha25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 25: Используя определение степени числа, напишите цикл,
+//который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+void zadacha25 ()
+{
+    Console.WriteLine ("Введите число а и нажмите 'Enter' ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Введите число b и нажмите 'Enter'");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int result = a;
+
+    for (int i = 1; i < b; i++)
+    {
+        result *= a;
+    }
+    Console.WriteLine(result);
+}
+ zadacha25 ();
